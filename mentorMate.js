954ad80156409495496768e364b47fe7f8f689ff
ViktoriaHr <60931293+ViktoriaHr@users.noreply.github.com>
2020-02-14 15:47:20 +0200

    Update mentorMate.js

--- mentorMate.js
+++ mentorMate.js
@@ -1,4 +1,4 @@
-function verificationNumber(number) {
+function validateNumber(number) {
     if(typeof number !== 'number'){
         console.log('Please enter numbers only!');
     }else if (1 > number || number > 10000 || number % 2 === 0) {
@@ -14,7 +14,7 @@ function printMentorMateLogo(n) {
     let decreasingElementOneStep = n;
     let decreasingElementTwoSteps = n;
     let increasingElementTwoSteps = n;
-    for (let i = 1; i <= linesCounter / 2; i++) { // print the lines above the middile  of M
+    for (let i = 1; i <= linesCounter / 2; i++) { // prints the lines above the middile  of M
         console.log((dash.repeat(decreasingElementOneStep) +
             star.repeat(increasingElementTwoSteps) +
             dash.repeat(decreasingElementTwoSteps) +
@@ -26,7 +26,7 @@ function printMentorMateLogo(n) {
     }
     [increasingElementTwoSteps, decreasingElementTwoSteps] = [decreasingElementTwoSteps, increasingElementTwoSteps] // swapping the logic of the elements
 
-    for (let i = linesCounter / 2; i <= n; i++) { // prints the riles  below the middle of M
+    for (let i = linesCounter / 2; i <= n; i++) { // prints the lines  below the middle of M
         increasingElementTwoSteps += 2;
         decreasingElementTwoSteps -= 2;
         console.log((dash.repeat(decreasingElementOneStep) +
@@ -39,4 +39,4 @@ function printMentorMateLogo(n) {
         decreasingElementOneStep -= 1; 
     }
 }
-verificationNumber(7);
+validateNumber(7);
